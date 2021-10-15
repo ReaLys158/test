8d1330d8bdaa1dbaac37aedf66a511558dd5eaee
ReaLys158 <53450759+ReaLys158@users.noreply.github.com>
2021-10-15 21:39:41 +0300

    Update moonbeam.sh

--- moonbeam.sh
+++ moonbeam.sh
@@ -2,9 +2,11 @@
 
 sudo apt-get update
 
-mkdir /var/lib/alphanet-data && chown moonbase_service /var/lib/alphanet-data && cd /var/lib/alphanet-data && chmod +x /var/lib/alphanet-data/moonbeam && chmod -R /var/lib/alphanet-data/
+mkdir /var/lib/alphanet-data && chown moonbase_service /var/lib/alphanet-data && cd /var/lib/alphanet-data
 
 RESULT=$(curl --silent "https://api.github.com/repos/PureStake/moonbeam/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') && echo $RESULT && wget https://github.com/PureStake/moonbeam/releases/download/$RESULT/moonbeam
+
+chmod +x /var/lib/alphanet-data/moonbeam && chmod -R /var/lib/alphanet-data/
 
 cd ~
 
