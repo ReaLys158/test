696b02d8ea289ea113578852142f0166b72c8d92
ReaLys158 <53450759+ReaLys158@users.noreply.github.com>
2021-10-16 03:43:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 echo "installing"
 
-systemctl stop moonbeam.service && cd /var/lib/alphanet-data/ && rm -rf moonbeam && RESULT=$(curl --silent "https://api.github.com/repos/PureStake/moonbeam/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') && echo $RESULT && wget https://github.com/PureStake/moonbeam/releases/download/$RESULT/moonbeam && chmod +x /var/lib/alphanet-data/moonbeam && cd ~ && systemctl enable moonbeam.service && systemctl start moonbeam.service && echo -n > tut.log && journalctl -u moonbeam.service > /root/tut.log --since "1970-01-01" -n 30 --no-pager
+systemctl stop moonbeam.service && cd /var/lib/alphanet-data/ && rm -rf moonbeam && RESULT=$(curl --silent "https://api.github.com/repos/PureStake/moonbeam/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') && echo $RESULT && wget https://github.com/PureStake/moonbeam/releases/download/$RESULT/moonbeam && chmod +x /var/lib/alphanet-data/moonbeam && cd ~ && systemctl enable moonbeam.service && systemctl start moonbeam.service && echo -n > tut.log && journalctl -u moonbeam.service > /root/tut.log --since "1970-01-01" -n 30 --no-pager && cd ~
